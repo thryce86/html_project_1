2aac6f4581db88e3aec5e9b3fc2ca76977e5b962
chad moore <thryce85@tamu.edu>
2022-02-23 18:13:34 -0600

    added js features for Read06

---
+++ app1.js
@@ -0,0 +1,9 @@
+let userInput = prompt('Do you like kittens?\n Y or y ') ;
+
+// let possibleInput = {'Y' , 'y' , 'yes', "Yes"};
+if(userInput == "y"|| userInput == "y"){
+    document.write('You are correct');
+}else{
+    document.write('Incorrect')
+}
+confirm("Press a button!");
---
+++ app2.js
@@ -0,0 +1,10 @@
+let userInput = prompt('Do you like kittens?\n Y or y ') ;
+
+// let possibleInput = {'Y' , 'y' , 'yes', "Yes"};
+if(userInput == "y"|| userInput == "y"){
+    document.write('You are correct');
+}else{
+    document.write('Incorrect');
+    
+}
+confirm('Reallyyy if you put no that is \n Disgraceful....');
